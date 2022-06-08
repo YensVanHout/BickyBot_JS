331ff6f8cd452516f4276c93ad54145e9fd83ccf
Yens Van Hout <yens.vanhout@student.ap.be>
2022-06-08 11:46:20 +0200

    Added basic commands

--- index.ts
+++ index.ts
@@ -8,12 +8,65 @@ const client = new DiscordJS.Client({
 
 client.on("ready", () => {
   console.log("bot is up and running");
+
+  const guildId = "952899403436404736";
+  const guild = client.guilds.cache.get(guildId);
+
+  let commands;
+
+  if (guild) {
+    commands = guild.commands;
+  } else {
+    commands = client.application?.commands;
+  }
+
+  commands?.create({
+    name: "bb",
+    description:
+      "Gebruik deze command in combinatie met een subcommand om Bicky Bot te gebruiken",
+    options: [
+      {
+        name: "subcommand",
+        description: "help for all possible subcommands",
+        required: true,
+        type: DiscordJS.Constants.ApplicationCommandOptionTypes.STRING,
+      },
+    ],
+  });
+});
+
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isCommand()) {
+    return;
+  }
+
+  const { commandName, options } = interaction;
+
+  if (commandName === "bb") {
+    const param = options.getString("subcommand");
+
+    if (param === "help") {
+      interaction.reply({
+        content:
+          "Bicky Bot ondersteunt momenteel de volgende commands:\n`!bb help`: Haal alle huidige commands op. (dit bericht)\n`!bb socials`: Krijg de huidige social media links van Bickychips.",
+        ephemeral: true,
+      });
+    }
+
+    if (param === "socials") {
+      interaction.reply({
+        content:
+          "Op deze platformen kan je BickyChips vinden:\nInstagram: https://www.instagram.com/bickychips/\nTwitch: https://www.twitch.tv/bickychips_\nTwitter: https://twitter.com/KellGraphy\nDiscord: https://discord.gg/uBDTGuMhqQ\nTiktok: https://www.tiktok.com/@bickychips",
+        ephemeral: true,
+      });
+    }
+  }
 });
 
 client.on("messageCreate", (message) => {
-  if (message.content === "ping") {
+  if (message.content.toLowerCase().includes("welk")) {
     message.reply({
-      content: "pong",
+      content: "das hier wel in het antwaarps he",
     });
   }
 });
