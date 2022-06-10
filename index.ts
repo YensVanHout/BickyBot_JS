275c324d9d8a3af6c8f7e2474bcee142db199187
Yens Van Hout <yens.vanhout@student.ap.be>
2022-06-10 11:00:05 +0200

    added wokcommands, added mongodb, removed discordjs vanilla commands

--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
 import DiscordJS, { Intents } from "discord.js";
 
-import { getQuote } from "./quotes";
+import WOKCommands from "wokcommands";
+import path from "path";
+
+import mongoose from "mongoose";
 
 const express = require("express");
 const app = express();
@@ -13,91 +16,26 @@ app.listen(port, () =>
 );
 
 const client = new DiscordJS.Client({
-  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+  ],
 });
 
-client.on("ready", () => {
-  console.log("bot is up and running");
-
-  const guildId = "948213338150694932";
-  const guild = client.guilds.cache.get(guildId);
-
-  let commands;
-
-  if (guild) {
-    commands = guild.commands;
-  } else {
-    commands = client.application?.commands;
-  }
-
-  commands?.create({
-    name: "bb",
-    description:
-      "Gebruik deze command in combinatie met een subcommand om Bicky Bot te gebruiken",
-    options: [
-      {
-        name: "subcommand",
-        description: "help for all possible subcommands",
-        required: true,
-        type: DiscordJS.Constants.ApplicationCommandOptionTypes.STRING,
-      },
-    ],
+client.on("ready", async () => {
+  await mongoose.connect(process.env["MONGODB"] || "", {
+    keepAlive: true,
   });
 
-  commands?.create({
-    name: "test",
-    description: "test the bot",
-  });
-});
-
-client.on("interactionCreate", async (interaction) => {
-  if (!interaction.isCommand()) {
-    return;
-  }
-
-  const { commandName, options } = interaction;
-
-  if (commandName === "bb") {
-    const param = options.getString("subcommand");
-
-    if (param === "help") {
-      interaction.reply({
-        content:
-          "Bicky Bot ondersteunt momenteel de volgende commands:\n`/bb help`: Haal alle huidige commands op. (dit bericht)\n`/bb socials`: Krijg de huidige social media links van Bickychips.",
-        ephemeral: true,
-      });
-    }
-
-    if (param === "socials") {
-      interaction.reply({
-        content:
-          "Op deze platformen kan je BickyChips vinden:\nInstagram: https://www.instagram.com/bickychips/\nTwitch: https://www.twitch.tv/bickychips_\nTwitter: https://twitter.com/KellGraphy\nDiscord: https://discord.gg/uBDTGuMhqQ\nTiktok: https://www.tiktok.com/@bickychips",
-        ephemeral: true,
-      });
-    }
-
-    if (param === "quote") {
-      interaction.reply({
-        content: getQuote(),
-        ephemeral: true,
-      });
-    }
-  }
-
-  if (commandName === "test") {
-    interaction.reply({
-      content: "test",
-      ephemeral: true,
-    });
-  }
-});
-
-client.on("messageCreate", (message) => {
-  if (message.content.toLowerCase().includes("welk")) {
-    message.reply({
-      content: "das hier wel in het antwaarps he",
-    });
-  }
+  new WOKCommands(client, {
+    commandsDir: path.join(__dirname, "commands"),
+    typeScript: true,
+    testServers: ["948213338150694932"],
+    botOwners: ["155784814551629826"],
+  })
+    .setDefaultPrefix("!")
+    .setDisplayName("Bicky Bot");
 });
 
 client.login(process.env["TOKEN"]);
